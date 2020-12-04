0c58f49d92c80a51c5e90fe0ebf4bc76e6ef8701
Andrew Borley <borley@uk.ibm.com>
2020-12-04 21:06:04 +0000

    fix: Refactor to Topic panel
     - Addresses initial review comments.
    
    Contributes to: #124
    
    Signed-off-by: Andrew Borley <borley@uk.ibm.com>

--- client/Panels/index.ts
+++ client/Panels/index.ts
@@ -2,6 +2,5 @@
  * Copyright Strimzi authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-export * from './Hook';
-export * from './useTopics.types';
-export * from './useTopics.assets';
+export * from './Home';
+export * from './Topics';
