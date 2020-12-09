7f251e6b55fdd9182fecc6bed0d689068400f917
Pete Muir <pmuir@bleepbleep.org.uk>
2020-12-09 10:15:25 +0000

    fix: fix a merge error that removed the bootstrap
    Signed-off-by: Pete Muir <pmuir@bleepbleep.org.uk>

--- client/Bootstrap/bootstrap.tsx
+++ client/Bootstrap/bootstrap.tsx
@@ -2,10 +2,25 @@
  * Copyright Strimzi authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-import { init } from 'i18n';
+import {init} from 'i18n';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { Home } from 'Panels/Home';
+import {ApolloProvider} from "@apollo/client";
+import {apolloClient} from "./GraphQLClient";
+import {ConfigFeatureFlagProvider, FeatureFlag} from "../Contexts/ConfigFeatureFlag";
+import {LoggingProvider} from "../Contexts/Logging";
+import {Topics} from "../Panels/Topics";
 
 init(); //Bootstrap i18next support
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(
+    <ApolloProvider client={apolloClient}>
+        <ConfigFeatureFlagProvider>
+            <LoggingProvider>
+                <FeatureFlag flag={'client.Pages.PlaceholderHome'}>
+                    <Topics/>
+                </FeatureFlag>
+            </LoggingProvider>
+        </ConfigFeatureFlagProvider>
+    </ApolloProvider>,
+    document.getElementById('root')
+);
