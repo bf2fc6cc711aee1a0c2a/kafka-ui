5656bec250be189c00cd7c750752b3d76ebdfb65
Donald Labaj <dlabaj@redhat.com>
2021-02-07 09:34:56 -0500

    Added mock api.

--- server/mockopenapi/handlers.ts
+++ server/mockopenapi/handlers.ts
@@ -1,5 +1,5 @@
-import {Request} from 'express'
-import {Topic, TopicsList} from './data/topic.types'
+import { Request } from 'express'
+import { Topic, TopicsList } from './data/topic.types'
 
 let topics: Array<Topic> = [
   {
@@ -43,50 +43,50 @@ let topics: Array<Topic> = [
 export const handlers = {
   getTopicsList: async (c, req, res) => {
     const topicList: TopicsList = {
-      count:topics.length,
+      count: topics.length,
       limit: topics.length,
-      offset:0,
+      offset: 0,
       topics: topics
     }
     res.status(202).json(topicList);
   },
 
-  deleteTopic: async (c, req: Request, res) =>{
-      
-      let newTopics: Topic[] = topics.filter((topic)=>{return topic.name !== req.params.topicName})
-      if (topics.length === newTopics.length) {
-        res.status(404).json({status: 404, err: "Topic not found."})
-      } else {
-        topics = newTopics;
-        res.status(200).json({status: 200, msg: "Topic deleted successfully"})
-      }
+  deleteTopic: async (c, req: Request, res) => {
+
+    let newTopics: Topic[] = topics.filter((topic) => { return topic.name !== req.params.topicName })
+    if (topics.length === newTopics.length) {
+      res.status(404).json({ status: 404, err: "Topic not found." })
+    } else {
+      topics = newTopics;
+      res.status(200).json({ status: 200, msg: "Topic deleted successfully" })
+    }
   },
 
   getTopic: async (c, req, res) => {
-    let currentTopic = topics.find((topic)=>{return topic.name === req.params.topicName})
+    let currentTopic = topics.find((topic) => { return topic.name === req.params.topicName })
     if (currentTopic === undefined) {
-      res.status(404).json({status: 404, err: "Topic not found."})
+      res.status(404).json({ status: 404, err: "Topic not found." })
     } else {
       res.status(200).json(currentTopic);
     }
   },
 
-  updateTopic: async (c, req:Request, res) => {
+  updateTopic: async (c, req: Request, res) => {
     let topicUpdated = false;
-    topics.forEach((topic, index:number, topics: Topic[])=>{
+    topics.forEach((topic, index: number, topics: Topic[]) => {
       if (topic.name === req.params.topicName) {
         topics[index] = req.body
-      } 
-      })
+      }
+    })
     if (topicUpdated) {
-      res.status(200).json({status: 200, msg: "Updated topic"});
+      res.status(200).json({ status: 200, msg: "Updated topic" });
     } else {
-      res.status(404).json({status: 404, err: "Topic not found."});
+      res.status(404).json({ status: 404, err: "Topic not found." });
     }
   },
-  
+
   // Handling auth
-  notFound: async (c, req, res) => res.status(404).json({status: 404, err: "not found" }),
+  notFound: async (c, req, res) => res.status(404).json({ status: 404, err: "not found" }),
   unauthorizedHandler: async (c, req, res) =>
     res.status(401).json({ err: "unauthorized" }),
 };
