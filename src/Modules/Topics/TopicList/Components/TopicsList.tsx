6962c1f5e5e417a34cc593263b850c09c6f246c6
suyash-naithani <suyash.naithani@gmail.com>
2021-02-16 18:38:59 +0530

    Fixed indentation and added licence

--- src/Modules/Topics/TopicList/Components/TopicsList.tsx
+++ src/Modules/Topics/TopicList/Components/TopicsList.tsx
@@ -174,7 +174,6 @@ export const TopicsListComponent: React.FunctionComponent<ITopicList> = ({
   }
 
   return (
-    <>
      <Page>
        <PageSection>
       {deleteModal && (
@@ -240,8 +239,8 @@ export const TopicsListComponent: React.FunctionComponent<ITopicList> = ({
       <Divider />
       {rowData.length < 1 && search.length > 1 && <EmptySearch />}
       {rowData.length > 1 && (
-        <Card>
-        <Pagination
+          <Card>
+          <Pagination
           itemCount={rowData.length}
           perPage={perPage}
           page={page}
@@ -253,9 +252,7 @@ export const TopicsListComponent: React.FunctionComponent<ITopicList> = ({
         />
         </Card>
       )}
-       
-      </PageSection>
-      </Page>
-    </>
+        </PageSection>
+        </Page>      
   );
 };
