4e74093f0904f51cd72d3c39f8171ec7728f49c4
Pete Muir <pmuir@bleepbleep.org.uk>
2021-03-01 11:20:02 +0000

    fix: failing tests

--- client/Elements/Components/Topics/TopicsList.steps.tsx
+++ client/Elements/Components/Topics/TopicsList.steps.tsx
@@ -4,11 +4,30 @@
  */
 
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import { TopicsListComponent } from './TopicsList.patternfly';
 
+import { Topic, TopicsList } from '../../../OpenApi';
+import { IConfiguration } from '../../../Contexts';
+
+jest.mock('../../../Services');
+import { getTopics } from '../../../Services';
+
 describe('<TopicsList />', () => {
-  it('should render a list of topics', () => {
+  it('should render a list of topics', async () => {
+    const topics = {
+      items: [
+        {
+          name: 'foo',
+        },
+      ] as Topic[],
+    } as TopicsList;
+
+    const getTopicsMock = getTopics as jest.MockedFunction<
+      (config: IConfiguration | undefined) => Promise<TopicsList>
+    >;
+    getTopicsMock.mockReturnValueOnce(Promise.resolve(topics));
+
     const { getByText } = render(
       <TopicsListComponent
         onCreateTopic={() => {
@@ -17,9 +36,11 @@ describe('<TopicsList />', () => {
       />
     );
 
+    await waitFor(() => expect(getByText('Topics')).toBeInTheDocument());
+
     const titleNode = getByText('Topics');
 
-    const createBtn = getByText('Create Topic');
+    const createBtn = getByText('Create topic');
 
     expect(titleNode).toBeInTheDocument();
 
