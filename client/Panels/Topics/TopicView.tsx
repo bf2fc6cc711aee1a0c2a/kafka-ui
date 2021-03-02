441da5239cc0d767d9fcb666313a9a8d26198c37
suyash-naithani <suyash.naithani@gmail.com>
2021-03-02 19:56:24 +0530

    Fix tests and address review comments

--- client/Panels/Topics/TopicView.tsx
+++ client/Panels/Topics/TopicView.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 
 import './style.scss';
 import '@patternfly/react-core/dist/styles/base.css';
-import { TabHeaders } from 'Modules//OpenShiftStreams/TabHeaders/TabHeaders';
+import { TabHeaders } from 'Modules/OpenShiftStreams/TabHeaders/TabHeaders';
 const Topics: FunctionComponent = () => {
   return <TabHeaders eventKey={1} />;
 };
