06ddc5f906b64aaf526d20349590ea6bbb03eca5
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2021-03-02 18:58:21 +0530

    Fix: handle form submit for basic topic create

--- client/Modules/Topics/CreateTopic/Components/StepTopicName.tsx
+++ client/Modules/Topics/CreateTopic/Components/StepTopicName.tsx
@@ -20,36 +20,37 @@ export const StepTopicName: React.FC<IStepTopicName> = ({
   const handleTopicNameChange = (topicNameInput) => {
     setTopicNameInput(topicNameInput);
   };
+
+  const preventFormSubmit = (event) => event.preventDefault();
+
   return (
-    <>
-      <TextContent className='topics-wizard-content'>
-        <Text component={TextVariants.h2}>Topic name</Text>
-        <Text component={TextVariants.p}>
-          This is the unique name used to recognize your topic.
+    <TextContent className='topics-wizard-content'>
+      <Text component={TextVariants.h2}>Topic name</Text>
+      <Text component={TextVariants.p}>
+        This is the unique name used to recognize your topic.
         </Text>
-        <Text component={TextVariants.small}>
-          It will also be used by your producers and consumers as part of the
-          connection information, so make it something easy to recognize.
+      <Text component={TextVariants.small}>
+        It will also be used by your producers and consumers as part of the
+        connection information, so make it something easy to recognize.
         </Text>
-        <Form>
-          <FormGroup
-            label='Topic name'
-            fieldId='step-topic-name-form'
-            // helperText="Please enter your topic name"
-          >
-            <TextInput
-              isRequired
-              type='text'
-              id='step-topic-name-input'
-              name='step-topic-name'
-              aria-describedby='step-topic-name-helper'
-              value={topicNameInput}
-              onChange={handleTopicNameChange}
-              placeholder='Enter topic name'
-            />
-          </FormGroup>
-        </Form>
-      </TextContent>
-    </>
+      <Form onSubmit={preventFormSubmit}>
+        <FormGroup
+          label='Topic name'
+          fieldId='step-topic-name-form'
+        // helperText="Please enter your topic name"
+        >
+          <TextInput
+            isRequired
+            type='text'
+            id='step-topic-name-input'
+            name='step-topic-name'
+            aria-describedby='step-topic-name-helper'
+            value={topicNameInput}
+            onChange={handleTopicNameChange}
+            placeholder='Enter topic name'
+          />
+        </FormGroup>
+      </Form>
+    </TextContent>
   );
 };
