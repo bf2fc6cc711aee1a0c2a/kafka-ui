55ed1c6dbc5c9d8326d21f3534232b6a84219c2a
suyash-naithani <suyash.naithani@gmail.com>
2021-03-02 17:53:08 +0530

    Fix tests

--- src/Panels/Topics/TopicView.tsx
+++ src/Panels/Topics/TopicView.tsx
@@ -1,17 +1,10 @@
-import React, { FunctionComponent } from 'react';
-import { useHistory } from 'react-router';
-import './style.scss';
-import '@patternfly/react-core/dist/styles/base.css';
-import { TopicsListComponent } from '../../Modules/Topics/TopicList/Components/TopicsList';
-import {TabHeaders} from "../../Modules/OpenShiftStreams/TabHeaders/TabHeaders"
+import React, { FunctionComponent } from "react";
+
+import "./style.scss";
+import "@patternfly/react-core/dist/styles/base.css";
+import { TabHeaders } from "../../Modules/OpenShiftStreams/TabHeaders/TabHeaders";
 const Topics: FunctionComponent = () => {
-  const history = useHistory();
-
-  const onCreateTopic = () => {
-    history.push('/topics/create');
-  };
-
-  return <TabHeaders eventKey={1}/>;
+  return <TabHeaders eventKey={1} />;
 };
 
 export { Topics };
