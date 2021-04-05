7df65e2fec3a6978011f31db26f5f9868edceeb2
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2021-04-05 18:06:25 +0530

    feat: add methods to findById and reset offset

--- mock-server/src/strimzi-api-handlers.js
+++ mock-server/src/strimzi-api-handlers.js
@@ -10,6 +10,47 @@ module.exports = {
       items: consumerGroups
     })
   },
+  getConsumerGroupById: async (c, req, res) => {
+    const group = getConsumerGroup(c.request.params.consumerGroupId)
+    if (!group) {
+      return res.status(404).json({ err: "not found" })
+    }
+    return res.status(200).json(group)
+  },
+  deleteConsumerGroupById: async (c, req, res) => {
+    const id = c.request.params.consumerGroupId;
+
+    const group = getConsumerGroup(id)
+    if (!group) {
+      return res.status(404).json({ err: "not found" })
+    }
+    consumerGroups = consumerGroups.filter(t => t.id !== id);
+
+    return res.status(204).json({ message: 'deleted' })
+  },
+  resetConsumerGroupOffset: async (c, req, res) => {
+    const id = c.request.params.consumerGroupId;
+
+    const group = getConsumerGroup(id)
+    if (!group) {
+      return res.status(404).json({ err: "not found" })
+    }
+    if (group.consumers && group.consumers.length) {
+      for (let i = 0; i < group.consumers.length; i++) {
+        group.consumers[i] = {
+          ...group.consumers[i],
+          offset: 0,
+          lag: 0,
+          logEndOffset: 0
+        }
+      }
+    }
+    consumerGroups = consumerGroups.filter(t => t.id !== id);
+    consumerGroups.push(group);
+
+    return res.status(200).json(group);
+
+  },
   createTopic: async (c, req, res) => {
     const topicBody = c.request.body;
 
