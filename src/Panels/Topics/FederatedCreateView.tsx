b404ee6c4560f714a3a69c28ac6927a816bd85f1
Sarah Rambacher <srambach@redhat.com>
2021-04-12 11:58:38 -0400

    fix lint

--- src/Panels/Topics/FederatedCreateView.tsx
+++ src/Panels/Topics/FederatedCreateView.tsx
@@ -3,8 +3,6 @@ import './style.scss';
 import { ConfigContext, IConfiguration } from '../../Contexts';
 import {
   AlertVariant,
-  PageSection,
-  PageSectionVariants,
 } from '@patternfly/react-core';
 import { CreateTopicPage } from '../../Modules/Topics/CreateTopic/CreateTopicPage';
 import kafkai18n from '../../i18n';
@@ -39,7 +37,7 @@ const FederatedCreateTopic: FunctionComponent<FederatedCreateTopicProps> = ({
         value={{ basePath: apiBasePath, getToken } as IConfiguration}
       >
         <AlertContext.Provider value={alertContext}>
-            <CreateTopicPage setIsCreateTopic={setIsCreateTopic} />
+          <CreateTopicPage setIsCreateTopic={setIsCreateTopic} />
         </AlertContext.Provider>
       </ConfigContext.Provider>
     </I18nextProvider>
