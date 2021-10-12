8af264fbeb4d4f72a6b4422842cb673060066e29
ajaypratap003 <ajay.pratap233@gmail.com>
2021-10-12 12:24:15 +0530

    fix detele instance modal

--- src/modules/Topics/pages/MainView/MainViewFederated.tsx
+++ src/modules/Topics/pages/MainView/MainViewFederated.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { useHistory } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import {
   FederatedContext,
@@ -10,9 +11,16 @@ import kafkai18n from '@app/i18n';
 import { MainView } from './MainView';
 import { KafkaModalLoader, PaginationProvider } from '@app/components';
 import { ModalProvider } from '@rhoas/app-services-ui-components';
+import {
+  useModal,
+  ModalType,
+  DeleteInstanceProps,
+  useBasename,
+} from '@rhoas/app-services-ui-shared';
 
 export type MainViewFederatedProps = FederatedProps &
-  IConfiguration & {
+  IConfiguration &
+  Pick<DeleteInstanceProps, 'kafka'> & {
     apiBasePath: string;
   };
 
@@ -26,7 +34,28 @@ const MainViewFederated: FunctionComponent<MainViewFederatedProps> = ({
   setIsOpenDeleteInstanceModal,
   showMetrics,
   activeTab = 1,
+  kafka,
 }) => {
+  const history = useHistory();
+  const { getBasename } = useBasename();
+  const basename = getBasename();
+  const { showModal } = useModal<ModalType.KasDeleteInstance>();
+
+  const redirectAfterDeleteInstance = () => {
+    if (basename) {
+      //redirect on kafkas page i.e. /streams/kafkas
+      history.push(basename.replace(`/${kafka.id}`, ''));
+    }
+  };
+
+  const onDeleteInstance = () => {
+    showModal &&
+      showModal(ModalType.KasDeleteInstance, {
+        kafka,
+        onDelete: redirectAfterDeleteInstance,
+      });
+  };
+
   return (
     <I18nextProvider i18n={kafkai18n}>
       <ConfigContext.Provider value={{ basePath: apiBasePath, getToken }}>
@@ -43,7 +72,7 @@ const MainViewFederated: FunctionComponent<MainViewFederatedProps> = ({
         >
           <ModalProvider>
             <PaginationProvider>
-              <MainView />
+              <MainView onDeleteInstance={onDeleteInstance} />
               <KafkaModalLoader />
             </PaginationProvider>
           </ModalProvider>
