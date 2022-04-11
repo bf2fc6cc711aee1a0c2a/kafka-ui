1e158e4db5cb0770551796dc3b33f1b61d0a0b14
HemaHG <53568062+HemaHG@users.noreply.github.com>
2022-04-11 16:27:38 +0530

    fix: Enable Kebab menu in the topic page consumer group (#720)

--- src/modules/ConsumerGroups/components/ConsumerGroupState.tsx
+++ src/modules/ConsumerGroups/components/ConsumerGroupState.tsx
@@ -4,21 +4,21 @@ import { ConsumerGroupState as ConsumerGroupStateEnum } from '@rhoas/kafka-insta
 export const ConsumerGroupState = (
   state: ConsumerGroupStateEnum | undefined
 ) => {
-  const { t } = useTranslation(['kafka']);
+  const { t } = useTranslation(['kafkaTemporaryFixMe']);
 
   switch (state) {
     case ConsumerGroupStateEnum.Stable:
-      return t('consumerGroup.stable_state');
+      return t('consumerGroup.state.stable_state');
     case ConsumerGroupStateEnum.Empty:
-      return t('consumerGroup.empty_state');
+      return t('consumerGroup.state.empty_state');
     case ConsumerGroupStateEnum.Dead:
-      return t('consumerGroup.dead_state');
+      return t('consumerGroup.state.dead_state');
     case ConsumerGroupStateEnum.CompletingRebalance:
-      return t('consumerGroup.completing_rebalance_state');
+      return t('consumerGroup.state.completing_rebalance_state');
     case ConsumerGroupStateEnum.PreparingRebalance:
-      return t('consumerGroup.preparing_rebalance_state');
+      return t('consumerGroup.state.preparing_rebalance_state');
     case ConsumerGroupStateEnum.Unknown:
-      return t('consumerGroup.unknown_state');
+      return t('consumerGroup.state.unknown_state');
     default:
       return null;
   }
