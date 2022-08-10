a3d183cc2b4b494269e540e2c2dd82e21b716710
Ajay Pratap <ajaypratap003@users.noreply.github.com>
2022-08-10 14:54:38 +0530

    fix resetOffset dropdown selection issue (#901)
    Co-authored-by: Ajay Pratap <apratap@redhat.com>

--- src/components/DropdownWithToggle/DropdownWithToggle.tsx
+++ src/components/DropdownWithToggle/DropdownWithToggle.tsx
@@ -41,26 +41,21 @@ export const DropdownWithToggle: React.FC<IDropdownWithToggleProps> = ({
 
   const onSelect: DropdownProps['onSelect'] = (e) => {
     if (e && e.currentTarget.textContent) {
-      const value: string = e.currentTarget.textContent;
+      let value: string = e.currentTarget.textContent;
       if (onSelectOption && value) {
-        onSelectOption(value.toLowerCase(), name);
+        value = isLabelAndValueNotSame ? value.toLowerCase() : value;
+        onSelectOption(value, name);
       }
       setIsOpen((isOpen) => !isOpen);
     }
   };
 
   const getItems = (options: IDropdownOption[]) => {
-    const items = options.map((option) => {
-      const { key, value, label } = option;
-
-      return (
-        <DropdownItem key={key} value={value}>
-          {label || value}
-        </DropdownItem>
-      );
-    });
-
-    return items;
+    return options.map(({ key, value, label }) => (
+      <DropdownItem key={key} value={value}>
+        {label || value}
+      </DropdownItem>
+    ));
   };
 
   const getSelectedValue = () => {
