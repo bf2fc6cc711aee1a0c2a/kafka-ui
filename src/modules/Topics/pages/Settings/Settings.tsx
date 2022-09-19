d609346a232d5b66fc3f26fcbe904fc88fd5ae1c
hemahg <hhg@redhat.com>
2022-09-19 17:03:00 +0530

    handle instance drawer opening and incorporate review comments
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/modules/Topics/pages/Settings/Settings.tsx
+++ src/modules/Topics/pages/Settings/Settings.tsx
@@ -2,6 +2,7 @@ import { PageSection } from '@patternfly/react-core';
 import React from 'react';
 import { MainViewHeader } from '@app/components';
 import { FederatedProps, FederatedContext } from '@app/contexts';
+import { useModal, ModalType } from '@rhoas/app-services-ui-shared';
 
 export type SettingsProps = FederatedProps;
 
@@ -9,15 +10,28 @@ const Settings: React.FC<SettingsProps> = ({
   showSettings,
   kafkaName,
   kafkaPageLink,
+  handleInstanceDrawer,
+  redirectAfterDeleteInstance,
   kafka,
 }) => {
+  const { showModal } = useModal<ModalType.KasDeleteInstance>();
+
+  const onDeleteInstance = () => {
+    showModal &&
+      showModal(ModalType.KasDeleteInstance, {
+        kafka,
+        onDelete: redirectAfterDeleteInstance,
+      });
+  };
   return (
     <FederatedContext.Provider
       value={{
         kafkaName,
         kafkaPageLink,
         kafka,
-        showSettings,
+        handleInstanceDrawer,
+        redirectAfterDeleteInstance,
+        onDeleteInstance,
       }}
     >
       <MainViewHeader activeTabKey={5} />
