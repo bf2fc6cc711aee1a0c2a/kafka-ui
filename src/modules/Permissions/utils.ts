f0e1059b6af067d6c7e2b1256c084f90763478b4
suyash-naithani <suyash.naithani@gmail.com>
2022-11-15 15:09:40 +0530

    Add missing files

--- src/modules/Permissions/utils.ts
+++ src/modules/Permissions/utils.ts
@@ -1,6 +1,26 @@
 import { ResourceOperationValue } from '@rhoas/app-services-ui-components/types/src/Kafka/ManageKafkaPermissions/components/ResourceOperation';
 import { ResourceTypeValue } from '@rhoas/app-services-ui-components/types/src/Kafka/ManageKafkaPermissions/components/ResourceType';
 import { AclResourceType } from '@rhoas/kafka-instance-sdk';
+import { sentenceCase } from 'sentence-case';
+
+export const displayName = (resourceType: AclResourceType): string => {
+  switch (resourceType) {
+    case AclResourceType.Group:
+      return 'Consumer group';
+
+    case AclResourceType.Topic:
+      return 'Topic';
+
+    case AclResourceType.Cluster:
+      return 'Kafka instance';
+
+    case AclResourceType.TransactionalId:
+      return 'Transactional ID';
+
+    default:
+      return sentenceCase(resourceType);
+  }
+};
 
 export const transformResourceType = (value: ResourceTypeValue | undefined) => {
   switch (value) {
