2b6fdea79f5b9ff842ac451b4a4e868671955674
hemahg <hhg@redhat.com>
2022-12-14 10:50:40 +0530

    Integrate topic table
    Signed-off-by: hemahg <hhg@redhat.com>

--- src/modules/Topics/Topics.tsx
+++ src/modules/Topics/Topics.tsx
@@ -1,62 +1,50 @@
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { Card, PageSection, PageSectionVariants } from '@patternfly/react-core';
-import { useTimeout } from '@app/hooks/useTimeOut';
-import { TopicsTable } from './components';
+import { Card } from '@patternfly/react-core';
 import {
-  EmptyState,
-  MASEmptyStateVariant,
-  MASLoading,
-  usePaginationParams,
-} from '@app/components';
-import { getTopics, OrderKey } from '@app/services';
+  KafkaTopics,
+  usePaginationSearchParams,
+  useSortableSearchParams,
+  useURLSearchParamsChips,
+} from '@rhoas/app-services-ui-components';
 import { ConfigContext, useFederated } from '@app/contexts';
-import { TopicsList } from '@rhoas/kafka-instance-sdk';
 import { useBasename } from '@rhoas/app-services-ui-shared';
 import './Topics.css';
-import { ISortBy, OnSort, SortByDirection } from '@patternfly/react-table';
+import { KafkaTopic } from '@rhoas/app-services-ui-components/types/src/Kafka/KafkaTopics/types';
+import { useTimeout } from '@app/hooks';
+import { getTopics, KafkaTopicsSortableColumns } from '@app/services';
 import { isAxiosError } from '@app/utils/axios';
-
-const sortableCols: { [key: number]: OrderKey } = {
-  0: OrderKey.name,
-  1: OrderKey.partitions,
-  2: OrderKey.retentionMs,
-  3: OrderKey.retentionSize,
-};
-
-export type ITopic = {
-  name: string;
-  replicas: number;
-  partitions: number;
-};
-
-export type ITopicProps = {
-  rows: ITopic[];
-};
 
 const Topics: React.FC = () => {
   const { onError } = useFederated() || {};
-  const { t } = useTranslation(['kafkaTemporaryFixMe']);
 
   const config = useContext(ConfigContext);
-  const { page = 1, perPage = 10, setPage } = usePaginationParams() || {};
   const history = useHistory();
   const { getBasename } = useBasename() || { getBasename: () => '' };
   const basename = getBasename();
 
-  const [topicsList, setTopicsList] = useState<TopicsList>();
-  const [searchTopicName, setSearchTopicName] = useState<string>('');
-  const [order, setOrder] = useState<SortByDirection>();
-  const [orderKey, setOrderKey] = useState<OrderKey>();
-  const [sortBy, setSortBy] = useState<ISortBy>({ index: 0, direction: 'asc' });
+  const [topicsList, setTopicsList] = useState<KafkaTopic[]>();
 
-  const onSearch = useCallback(
-    (value: string) => {
-      setSearchTopicName(value);
-      setPage && setPage(1);
+  const [count, setCount] = useState<number>();
+
+  const { page, perPage, setPagination, setPaginationQuery } =
+    usePaginationSearchParams();
+  const resetPaginationQuery = useCallback(
+    () => setPaginationQuery(1, perPage),
+    [perPage, setPaginationQuery]
+  );
+
+  const topicsChips = useURLSearchParamsChips('topics', resetPaginationQuery);
+  const [isColumnSortable, sort, sortDirection] = useSortableSearchParams(
+    KafkaTopicsSortableColumns,
+    {
+      name: 'TODO name',
+      partitions: 'TODO partitions',
+      'retention.bytes': 'TODO retention bytes',
+      'retention.ms': 'TODO retention ms',
     },
-    [setPage]
+    'name',
+    'desc'
   );
 
   const onClickCreateTopic = () => {
@@ -71,23 +59,18 @@ const Topics: React.FC = () => {
     onEditTopic && onEditTopic(topicName);
   };
 
-  const onSort: OnSort = (_event, index, direction) => {
-    setOrderKey(sortableCols[index]);
-    setOrder(direction);
-    setSortBy({ index, direction });
-  };
-
   const fetchTopic = useCallback(async () => {
     try {
-      await getTopics(
+      return await getTopics(
         config,
         page,
         perPage,
-        searchTopicName,
-        order,
-        orderKey
-      ).then((topics) => {
+        sort!,
+        sortDirection,
+        topicsChips.chips[0]
+      ).then(({ topics, count }) => {
         setTopicsList(topics);
+        setCount(count);
       });
     } catch (err) {
       //TODO: Update the api to allow suppress alerts if the application does not want to show them as well.
@@ -101,72 +84,39 @@ const Topics: React.FC = () => {
         onError(code, message);
       }
     }
-  }, [config, onError, order, orderKey, page, perPage, searchTopicName]);
+  }, [config, onError, page, perPage, sort, sortDirection, topicsChips.chips]);
 
   useEffect(() => {
     fetchTopic();
-  }, [searchTopicName, order, orderKey, page, perPage, fetchTopic]);
+  }, [topicsChips.chips, sort, sortDirection, page, perPage, fetchTopic]);
 
   useTimeout(() => fetchTopic(), 5000);
 
   return (
     <>
       <Card className='kafka-ui-m-full-height' data-ouia-page-id='tableTopics'>
-        {(() => {
-          switch (true) {
-            case topicsList?.items === undefined:
-              return (
-                <PageSection
-                  className='kafka-ui-m-full-height'
-                  variant={PageSectionVariants.light}
-                  padding={{ default: 'noPadding' }}
-                >
-                  <MASLoading />
-                </PageSection>
-              );
-            case topicsList &&
-              topicsList?.items?.length < 1 &&
-              searchTopicName.length < 1:
-              return (
-                <EmptyState
-                  emptyStateProps={{
-                    variant: MASEmptyStateVariant.NoItems,
-                    'data-ouia-page-id': 'emptyStateTopics',
-                  }}
-                  titleProps={{
-                    title: t('topic.empty_topics_title'),
-                  }}
-                  emptyStateBodyProps={{
-                    body: t('topic.empty_topics_body'),
-                  }}
-                  buttonProps={{
-                    title: t('topic.create_topic'),
-                    onClick: onClickCreateTopic,
-                    'data-testid': 'actionCreateTopic',
-                    ouiaId: 'button-create',
-                  }}
-                />
-              );
-            case topicsList?.items !== undefined:
-              return (
-                <TopicsTable
-                  total={topicsList?.total || 0}
-                  page={page}
-                  perPage={perPage}
-                  onCreateTopic={onClickCreateTopic}
-                  topicItems={topicsList?.items || []}
-                  filteredValue={searchTopicName}
-                  setFilteredValue={onSearch}
-                  refreshTopics={fetchTopic}
-                  onEdit={onEdit}
-                  onSort={onSort}
-                  sortBy={sortBy}
-                />
-              );
-            default:
-              return <></>;
-          }
-        })()}
+        <KafkaTopics
+          topics={topicsList || []}
+          getUrlFortopic={(row) => `${basename}/topics/${row.topic_name}`}
+          onDelete={(row) => onEdit(row.topic_name)}
+          onEdit={(row) => onEdit(row.topic_name)}
+          onSearchTopic={(value) => {
+            topicsChips.clear();
+            topicsChips.toggle(value);
+          }}
+          onClearAllFilters={topicsChips.clear}
+          topicName={topicsChips.chips}
+          onRemoveTopicChip={topicsChips.clear}
+          onRemoveTopicChips={topicsChips.clear}
+          onTopicLinkClick={(row) => {
+            row.topic_name;
+          }}
+          page={page}
+          itemCount={count || 0}
+          onPageChange={setPagination}
+          onCreateTopic={onClickCreateTopic}
+          isColumnSortable={isColumnSortable}
+        />
       </Card>
     </>
   );
